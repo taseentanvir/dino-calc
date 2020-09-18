b06dc4653aa7e5cf843ddda4a4e589da681a7d71
taseentanvir <taseentanvir@gmail.com>
2020-09-18 20:27:27 +0600

    refactor: :triangular_flag_on_post: i refactored a shit
    longer description
    
    list any breaking changes closed by this change

--- src/App.js
+++ src/App.js
@@ -4,34 +4,34 @@ import "./App.css";
 // custom imports
 import Home from "./components/Home.js";
 import FactorizationCalc from "./components/factorizationCalc/FactorizationCalc.js";
-import BMICalc from "./components/BMICalc/BMICalc.js"
+import BMICalc from "./components/BMICalc/BMICalc.js";
 import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Link,
-	Redirect,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
 } from "react-router-dom";
 
 function App() {
-	return (
-		<div className="App">
-			<Router>
-				<Switch>
-					<Route exact path="/home">
-						<Home />
-					</Route>
-					<Route exact path="/factorization-calc">
-						<FactorizationCalc />
-					</Route>
-					<Route exact path="/bmi-calc">
-						<BMICalc />
-					</Route>
-					<Redirect to="/home" />
-				</Switch>
-			</Router>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/factorization-calc">
+            <FactorizationCalc />
+          </Route>
+          <Route exact path="/bmi-calc">
+            <BMICalc />
+          </Route>
+          <Redirect to="/home" />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
